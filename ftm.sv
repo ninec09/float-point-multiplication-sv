4b65d548ed0b17ab8cab9e96a48b3176e2f48679
ninec09 <mehmetsucuk@icloud.com>
2023-01-01 21:45:54 +0300

    kod tamamlandi, test caselerin bazilarinda eksiklikler var ozel kosullarin if else ile yazilmasi gerekiyor.

--- ftm.sv
+++ ftm.sv
@@ -1,13 +1,16 @@
-module multiplier (input logic [22:0] a,b,output logic [47:0] product);
+/*
+    Project     :   Float Point Multiplication
+    Date        :   01.01.2023
+    Author      :   Mehmet Sucuk         
+*/
+
+
+
+module multiplier (input logic [31:0] a,b,output logic [47:0] product);
 logic [23:0] op_a, op_b;
 assign op_a = (|a[30:23]) ? {1'b1,a[22:0]} : {1'b0,a[22:0]};
-assign op_b = (|b[30:23]) ? {1'b1,b[22:0]} : {1'b0,b[22:0]};;
+assign op_b = (|b[30:23]) ? {1'b1,b[22:0]} : {1'b0,b[22:0]};
 assign product = op_a * op_b;
-endmodule
-
-
-module addition (input logic [7:0] exponent1, exponent2, output logic [7:0] y);
-assign y = exponent1 + exponent2 - 127;       
 endmodule
 
 
@@ -17,39 +20,61 @@ assign signOut = sign1 ^ sign2;
 endmodule
 
 
-module normalised (input logic [47:0] product, input logic [7:0] exponent, output logic [22:0] product_mantissa,output logic [7:0] exponentRes);
+module normalize (input logic [47:0] product, input logic [7:0] exponent_a,exponent_b, output logic [22:0] product_mantissa,output logic [7:0] exponentResult);
 logic normalised,round;
 logic [47:0] product_normalised;
-assign round = |product_normalised[22:0]; 
 assign normalised = product[47] ? 1'b1 : 1'b0;                 
 assign product_normalised = normalised ? product : product << 1;	
-
+assign round = |product_normalised[22:0]; 
 assign product_mantissa = product_normalised[46:24] + (product_normalised[23] & round); 
-assign exponentRes = exponent;      
+assign exponentResult = exponent_a + exponent_b - 8'd127+1;
 endmodule 	
 
-// yukardaki modulleri isledigim son modul.
+
 module fpMultiplication(input logic [31:0] x, y,output logic [31:0] fpmult);
-logic [7:0] expx, expy, exponent, expoRes;
-logic [22:0] floatOne, floatTwo, floatOut;
-logic signOne, signTwo, signOut;
-logic [47:0] mantissa;
-assign {expx, floatOne} = {x[30:23], x[22:0]};
-assign {expy, floatTwo} = {y[30:23], y[22:0]};
-assign signOne = x[31];
-assign signTwo = y[31];
-multiplier mult(floatOne, floatTwo, mantissa);                                                 
-addition add(expx, expy, exponent);                                 // exponent modulu Out.
-signCalculation signcalc(signOne, signTwo, signOut);                // isaret biti modulu Out.
-normalize norm(mantissa, exponent, floatOut, expoRes);              // normalize islemi Out.
-assign fpmult = {signOut,expoRes,floatOut};                         // modulun sonucu.
+logic [7:0]  exponent_x, exponent_y;
+logic [7:0]  exponentResult;
+logic [22:0] product_mantissa;
+logic sign_x, sign_y, signResult;
+logic [47:0] product;
+
+assign exponent_x = x[30:23]; 
+assign exponent_y = y[30:23]; 
+assign sign_x = x[31];
+assign sign_y = y[31];
+
+multiplier  mult(x, y, product);                                                                                                                                
+sign        signcalc(sign_x,sign_y,signResult);                               
+normalize   norm(product,exponent_x,exponent_y,product_mantissa,exponentResult);                    
+
+
+assign fpmult = {signResult,exponentResult,product_mantissa};                              
 endmodule 
 
 module testbench();
 logic [31:0] x, y, out;
 fpMultiplication multiplication(x,y,out);
 initial begin
-assign x = 32'b10111110100110011001100110011010;                    // -0.3 sayisinin gosterimi
-assign y = 32'b01000011111110100010000000000000; #10;               // 500.25 sayisinin gosterimi
+
+assign x = 32'b10111110100110011001100110011010;                   // -0.3 sayisinin gosterimi
+assign y = 32'b01000011111110100010000000000000;                   // 500.25 sayisinin gosterimi
+                                                                   // sonuc : 0xC3161334
+#10;
+
+assign x = 32'b01000000010010011001100110011010;                   // 3.15 sayisinin gosterimi
+assign y = 32'b11000001011001100011110101110001;                   // -14.39 sayisinin gosterimi
+                                                                   // sonuc : 0xC2355063
+#10;
+
+assign x = 32'b01000101100000000000000000000000;                   // 4096 sayisinin gosterimi
+assign y = 32'b01000101100000000000000000000000;                   // 4096 sayisinin gosterimi
+                                                                   // sonuc : 0x4B800000
+#10;
+
+assign x = 32'b00000000000000000000000000000000;                   // 0 sayisinin gosterimi
+assign y = 32'b00000000000000000000000000000000;                   // 0 sayisinin gosterimi
+                                                                   // sonuc : 0x00000000
+#10;
+
 end
 endmodule
